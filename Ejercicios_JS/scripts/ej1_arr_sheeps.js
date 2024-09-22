d9c9eb2d88458439d5c68ae8ee17c89a43d73ad0
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-09-21 23:06:08 -0600

    test: added JS sheep counting exercise for arrays

---
+++ Ejercicios_JS/scripts/ej1_arr_sheeps.js
@@ -0,0 +1,39 @@
+/*Consider an array/list of sheep where some sheep may be missing from their place.
+We need a function that counts the number of sheep present in the array (true means present).*/
+
+function countSheeps(sheep) {
+  let res = 0;
+  sheep.forEach(function (item) {
+    res = item ? res + 1 : res;
+  });
+  return res;
+}
+
+sheeps = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+console.log(countSheeps(sheeps));
--- Ejercicios_JS/scripts/ejercicio1.js
+++
@@ -1,5 +0,0 @@
-const a = 3;
-
-if (a === 3) {
-  console.log("Eligio");
-}
