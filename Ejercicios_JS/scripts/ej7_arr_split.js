047936885d8b43a300fe12dc1473ec1ee651ffa1
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-09-23 13:18:02 -0600

    test: added JS split exercise for arrays

---
+++ Ejercicios_JS/scripts/ej7_arr_split.js
@@ -0,0 +1,8 @@
+/* Write a function to split a string and convert it into an array of words. */
+const stringToArray = (string) => string.split(" ");
+
+const input1 = "Robin Singh";
+const input2 = "I love arrays they are my favorite";
+
+console.log(stringToArray(input1));
+console.log(stringToArray(input2));
