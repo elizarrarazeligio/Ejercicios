d2aa011ea08ac2e4a30bf5044c78092c9df32117
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-01 18:38:24 -0600

    test: added JS map exercise for arrays

---
+++ Ejercicios_JS/scripts/ej24_arr_map.js
@@ -0,0 +1,6 @@
+/* Given an array of integers, return a new array with each value doubled. */
+const maps = (x) => x.map((num) => num * 2);
+
+console.log(maps([1, 2, 3]));
+console.log(maps([4, 1, 1, 1, 4]));
+console.log(maps([4, 1, 1, 1, 4]));
