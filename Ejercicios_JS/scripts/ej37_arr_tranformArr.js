aa6d6d5d0958f705a4b7662d6140e27f9d31eb62
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-11 15:38:40 -0600

    test: added JS array filter exercise for arrays

--- Ejercicios_JS/scripts/ej37_arr_tranformArr.js
+++ Ejercicios_JS/scripts/ej37_arr_tranformArr.js
@@ -9,9 +9,9 @@ const map = function (arr, fn) {
   });
 };
 
-arr = [1, 2, 3];
+arr = [1, 2, 3, 4, 5, 6, 7, 8];
 function plus(n, i) {
-  //return n + i;
+  return n + i;
   //return n + 1;
   return 42;
 }
