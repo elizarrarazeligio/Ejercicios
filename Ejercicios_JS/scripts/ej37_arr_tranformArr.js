5509947b0657dc4c1348df9930c94f2e833844a1
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-11 15:17:47 -0600

    test: added JS array map transformation exercise for arrays

---
+++ Ejercicios_JS/scripts/ej37_arr_tranformArr.js
@@ -0,0 +1,19 @@
+/* Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method. */
+const map = function (arr, fn) {
+  return arr.map((item, index) => {
+    return fn(item, index);
+  });
+};
+
+arr = [1, 2, 3];
+function plus(n, i) {
+  //return n + i;
+  //return n + 1;
+  return 42;
+}
+
+console.log(map(arr, plus));
