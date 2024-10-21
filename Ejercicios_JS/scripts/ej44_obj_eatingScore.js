d24a657c522e98b5168867b1ace7f7740942d045
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-20 23:10:27 -0600

    fix: exercise 44 eating exercise for objects

--- Ejercicios_JS/scripts/ej44_obj_eatingScore.js
+++ Ejercicios_JS/scripts/ej44_obj_eatingScore.js
@@ -12,3 +12,29 @@ Write a function that helps you create a scoreboard. It takes as a parameter a l
   {name: "Habanero Hillary", chickenwings: 5 , hamburgers: 17, hotdogs: 11},
   {name: "Big Bob" , chickenwings: 20, hamburgers: 4, hotdogs: 11}
 ] */
+
+function scoreboard(whoAteWhat) {
+  let res = {};
+  let arr = whoAteWhat.map((item) => {
+    return (res = {
+      name: item.name,
+      score: item.chickenwings * 5 + item.hamburgers * 3 + item.hotdogs * 2,
+    });
+  });
+  return arr.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+const whoAteWhat = [
+  { name: "Billy The Beast", chickenwings: 17, hamburgers: 7, hotdogs: 8 },
+  { name: "Habanero Hillary", chickenwings: 5, hamburgers: 17, hotdogs: 11 },
+  { name: "Joey Jaws", chickenwings: 8, hamburgers: 8, hotdogs: 15 },
+  { name: "Big Bob", chickenwings: 20, hamburgers: 4, hotdogs: 11 },
+];
+
+const WhoAteWhat2 = [
+  { name: "Joey Jaws", chickenwings: 8, hamburgers: 8, hotdogs: 15 },
+  { name: "Big Bob", chickenwings: 20, hamburgers: 4, hotdogs: 11 },
+];
+
+console.log(scoreboard(whoAteWhat));
+console.log(scoreboard(WhoAteWhat2));
