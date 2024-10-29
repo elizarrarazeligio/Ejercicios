fcec087c0121cd8c5e2ba635af7b875f51d770b6
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-10-29 17:53:48 -0600

    test: added JS chained promises exercise for promises

---
+++ Ejercicios_JS/scripts/ej48_prom_chainPromise.js
@@ -0,0 +1,17 @@
+const newPromise = new Promise(function (resolve, reject) {
+  resolve("Un Mississippi");
+});
+
+function firstAction(value) {
+  return `${value}, dos Mississippi`;
+}
+
+function secondAction(value) {
+  return `${value}, tres Mississippi.`;
+}
+
+function thirdAction(value) {
+  console.log(value);
+}
+
+newPromise.then(firstAction).then(secondAction).then(thirdAction);
